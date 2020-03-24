042606276dbe011094293f52af4678975574b008
tepetopo <oiertrepiana@gmail.com>
2020-03-24 13:49:37 +0100

    echos los metodos de vehiculo

--- retog3/Vehiculo.java
+++ retog3/Vehiculo.java
@@ -45,40 +45,55 @@ abstract class Vehiculo {
 		System.out.println("SERIE:");
 		int serie =Console.readInt();
 		
-		PreparedStatement ps2 = conexion.prepareStatement("SELECT * FROM retog3.student;");
-		ps2.executeUpdate();
 		
-		PreparedStatement ps = conexion.prepareStatement("INSERT INTO retog3.student (VALORES) VALUES " + 
-				" ('" + Matricula + "', " + "'"+numBastidor + "',"+"'"+color+"', " + "'"+numAsientos + "', " + "'"+precio + "', " + "'"+serie + "');");	
+		String sql=("INSERT INTO retog3.vehiculo (VALORES) VALUES " + " ('" + Matricula + "', " + "'"+numBastidor + "',"+"'"+color+"', " + "'"+numAsientos + "', " + "'"+precio + "', " + "'"+serie + "');");
+		PreparedStatement ps = conexion.prepareStatement(sql);	
 		
 		int status = ps.executeUpdate();	
 		if(status != 0) {
 			System.out.println("Datos insertados.");
+			}
 		}
-	}
 
     
 	
 	public void venderVehiculo(char tipo, Connection conexion) throws SQLException {
 
 		System.out.println("Write some data:");
-		System.out.println("NAME:");
+		System.out.println("MATRICULA:");
 		String matricula = Console.readString();
-		System.out.println("SCHOOL:");
-		//TODO String school = Console.readString();
-		
-		PreparedStatement ps = conexion.prepareStatement("DELETE retog3.student (studentName, studentSchool) VALUES " + 
-				" ('" + matricula + "');");	
+		String sql =("DELETE retog3.vehiculo WHERE matricula ='"+matricula+"';");
+		PreparedStatement ps = conexion.prepareStatement(sql);
 		
 		int status = ps.executeUpdate();	
 		if(status != 0) {
-			System.out.println("Conexión completa.");
-			System.out.println("Los datos fueron INSERTADOS.");
+			System.out.println("Coche vendido.");
+		}
+	}
+	public void pintarVehiculo(char tipo, Connection conexion) throws SQLException {
+		System.out.println("Write data:");
+		System.out.println("MATRICULA:");
+		int matricula = Console.readInt();
+		System.out.println("COLOR:");
+		String color =Console.readString();
+		
+		String sql=("UPDATE vehiculo SET color = '" + color + "', esPintado = " + (isPainted = true)+" WHERE matricula ='"+ matricula +"';");
+		
+		PreparedStatement ps = conexion.prepareStatement(sql) ;	
+		int status = ps.executeUpdate();	
+		if(status != 0) {
+			System.out.println("Pintado.");
 		}
 	}
 
-	public void display() {
-		System.out.println(toString());
+	public void display(char tipo, Connection conexion) throws SQLException {
+		
+		String sql=("SELECT * FROM retog3.vehiculo ");
+		PreparedStatement ps = conexion.prepareStatement(sql) ;	
+		int status = ps.executeUpdate();
+		if(status != 0) {
+			System.out.println("Pintado.");
+		}
 	}
 	
 	@Override 
